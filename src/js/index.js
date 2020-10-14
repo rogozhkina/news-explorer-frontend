6e7d266c25fc5a6467fde019c7c5c594348fd9f1
taschunina <taschunina@yandex.ru>
2020-10-14 18:07:00 +0300

    Правка верстки

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
 import '../css/index.css';
+
+// const button = document.querySelector('.button__search');
+
+// console.log(button);
+
+// button.addEventListener('click', () => {
+//   button.classList.add('button_invisible');
+// });
