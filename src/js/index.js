bcaa9342881ec8ae09a3dc17cb2b593e6be61046
taschunina <taschunina@yandex.ru>
2020-10-15 17:47:55 +0300

    Частично работает попап аворизации

--- src/js/index.js
+++ src/js/index.js
@@ -9,11 +9,25 @@ import '../css/index.css';
 // });
 
 const popupMenu = document.querySelector('.popup_menu');
+const popupAuth = document.querySelector('.popup_authorization');
 const iconMenu = document.querySelector('.icon_menu');
 const overlay = document.querySelector('.overlay');
+const iconClose = document.querySelector('.popup__icon');
+const buttonAuth = document.querySelector('.button_type_registration');
 
 iconMenu.addEventListener('click', () => {
   // overlay.classList.add('overlay_show');
   popupMenu.classList.add('popup_menu_open');
   iconMenu.classList.add('icon_menu_hidden');
+});
+
+// iconClose.addEventListener('click', () => {
+//   console.log(iconClose);
+//   popupMenu.classList.remove('popup_menu_open');
+// });
+
+buttonAuth.addEventListener('click', () => {
+  console.log(buttonAuth);
+  console.log(popupAuth);
+  popupAuth.classList.add('popup_authorization_open');
 });
