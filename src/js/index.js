345d35931b38da0be43ba6c9ce534d20a01d56f4
taschunina <taschunina@yandex.ru>
2020-10-18 18:21:08 +0300

    Почищен js

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,8 @@
 import '../css/index.css';
 
-// const button = document.querySelector('.button__search');
-
-// console.log(button);
-
-// button.addEventListener('click', () => {
-//   button.classList.add('button_invisible');
-// });
-
 const popupMenu = document.querySelector('.popup_menu');
 const popupAuth = document.querySelector('.popup_authorization');
 const iconMenu = document.querySelector('.icon_menu');
-const overlay = document.querySelector('.overlay');
-const iconClose = document.querySelector('.popup__icon');
 const buttonAuthPopup = document.querySelector('.button_type_authorization_popup');
 const iconFlags = document.querySelectorAll('.article-card__icon');
 const buttonAuthMenu = document.querySelector('.button_type_authorization_menu');
@@ -22,15 +12,9 @@ const popupIcon = document.querySelector('.popup_icon');
 const preloader = document.querySelector('.preloader');
 
 iconMenu.addEventListener('click', () => {
-  // overlay.classList.add('overlay_show');
   popupMenu.classList.add('popup_menu_open');
   iconMenu.classList.add('icon_menu_hidden');
 });
-
-// iconClose.addEventListener('click', () => {
-//   console.log(iconClose);
-//   popupMenu.classList.remove('popup_menu_open');
-// });
 
 buttonAuthPopup.addEventListener('click', () => {
   popupAuth.classList.add('popup_authorization_open');
