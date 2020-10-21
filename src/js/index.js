27fe4257e46eb26ed5a8d173fcbfaec8b1724f5c
taschunina <taschunina@yandex.ru>
2020-10-21 10:47:23 +0300

    Шаблон карточки

--- src/js/index.js
+++ src/js/index.js
@@ -39,3 +39,63 @@ iconFlag.addEventListener('click', () => {
 buttonSearch.addEventListener('click', () => {
   preloader.classList.add('preloader_active');
 });
+
+class Card {
+  constructor(cardData) {
+    this._domElement = null;
+    this._cardData = cardData;
+    this._likeButton = null;
+    this.like = this.like.bind(this);
+  }
+
+  _createCard() {
+    const templateString = `
+
+    </div>
+
+<div class="article-card">
+            <div class="article-card__image-section">
+              <div class="article-card__image">
+                <div class="popup_icon"></div>
+                <button
+                  class="article-card__icon article-card__icon_special"
+                ></button>
+              </div>
+            </div>
+            <p class="article-card__date"></p>
+            <h2 class="article-card__title"></h2>
+            <p class="article-card__text">
+            </p>
+            <p class="article-card__source"></p>
+</div>`;
+
+    const template = document.createElement("div");
+    template.insertAdjacentHTML("beforeend", templateString.trim());
+
+    const placeCard = template.firstElementChild;
+    placeCard.querySelector(
+      ".article-card__title"
+    ).textContent = this._cardData.title;
+
+    const cardImage = placeCard.querySelector(".article-card__image");
+    cardImage.style.backgroundImage = `url(${this._cardData.urlToImage})`;
+
+
+    this._likeButton = placeCard.querySelector(".article-card__icon");
+    this._likeButton.addEventListener("click", this.like);
+
+    return placeCard;
+  }
+
+  domElement() {
+    if (null == this._domElement) {
+      this._domElement = this._createCard();
+    }
+    return this._domElement;
+  }
+
+  like() {
+    this._likeButton.classList.toggle("article-card__icon_special");
+  }
+
+}
