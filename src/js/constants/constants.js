69d912f45f00658248c9d1350e000a57d046ee4c
taschunina <taschunina@yandex.ru>
2021-01-18 19:04:39 +0300

    Константа количества карточек 3

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -2,3 +2,4 @@ export const errorEmptyField = 'Это обязательное поле';
 export const errorWrongLength = 'Должно быть от 2 до 30 символов';
 export const errorWrongLink = 'Это не ссылка';
 export const daysNumber = 7;
+export const showCardsNumber = 3;
